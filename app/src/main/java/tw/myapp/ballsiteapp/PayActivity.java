3e546d81b8f7d657b3bb4a33c273e6925e60275c
USER <k30023105@gmail.com>
2023-01-19 15:38:17 +0800

    增加日曆和付款畫面的Activity(CalendarActivity和PayActivity) 刪除那兩個Layout 修改選擇日期時間的畫面 刪除選擇球類的按鈕V1.0.2

---
+++ app/src/main/java/tw/myapp/ballsiteapp/PayActivity.java
@@ -0,0 +1,14 @@
+package tw.myapp.ballsiteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
