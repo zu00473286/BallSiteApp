53b65ab71b90a340083db26060b138f18f13bc27
zu00473286 <xiongxiao569@gmail.com>
2023-01-20 12:17:54 +0800

    新增選擇日期功能 v1.1.0

--- app/src/main/java/tw/myapp/ballsiteapp/VenueRentalActivity.java
+++ app/src/main/java/tw/myapp/ballsiteapp/VenueRentalActivity.java
@@ -2,9 +2,13 @@ package tw.myapp.ballsiteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 import tw.myapp.ballsiteapp.databinding.ActivityVenueRentalBinding;
 
@@ -16,7 +20,22 @@ public class VenueRentalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityVenueRentalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        binding.CalenderSelectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance(); //取得java內建的 日曆物件
+                int y = calendar.get(calendar.YEAR);
+                int m = calendar.get(calendar.MONTH);
+                int d = calendar.get(calendar.DATE);
+                DatePickerDialog dialog = new DatePickerDialog(VenueRentalActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        binding.CalenderSelectBtn.setText("日期:" + year + "/" + (month+1)+ "/" + dayOfMonth );
+                    }
+                }, y,m,d);
+                dialog.show();
+            }
+        });
 
         binding.OKBtn.setOnClickListener(new View.OnClickListener() {
             @Override
